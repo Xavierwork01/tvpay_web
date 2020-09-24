7103712ff0d70530cca115482919f91398436d16
xavier <xavierwork01@gmail.com>
2020-09-24 11:07:09 +0800

    9/24 購物車頁面完成

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -14,12 +14,12 @@ class ShoppingCart extends React.Component{
             <Layout>
                 <div className="shoppingcart">
                     <div className="shop-voucher">
-                        點我領取運費抵用券！！！ 領取優惠券
+                        <a href="#">點我領取運費抵用券！！！</a>
                     </div>
                     <div className="shop-header">
                         <div className="shop-item1">
-                            <input type="checkbox" />
-                            <div>商品</div>
+                            <div className="shop-car-chk"><input type="checkbox" /></div>
+                            <div className="shop-name">商品</div>
                         </div>
                         <div className="shop-item2">
                             <div>單價</div>
@@ -29,48 +29,90 @@ class ShoppingCart extends React.Component{
                         </div>
                     </div>
                     <div className="shop-car">
-                        <input type="checkbox"  />
+                        <div className="shop-car-chk"><input type="checkbox" /></div>
                         <div className="shop-img">
                             <img src=""/>
                         </div>
-                        <div className="shop-name">在台現貨/贈10禮 免運 可到付 酷貝樂/艾尚寶 高景觀 可坐可躺 雙向推</div>
+                        <div className="shop-text">在台現貨/贈10禮 免運 可到付 酷貝樂/艾尚寶 高景觀 可坐可躺 雙向推在台現貨/贈10禮 免運 可到付 酷貝樂/艾尚寶 高景觀 可坐可躺 雙向推</div>
                         <div className="shop-format">
                             規格
                         </div>
-                        <div className="shop-unitprice">$2450</div>
+                        <div className="shop-unitprice">$245000</div>
                         <div className="shop-mount">
-                            <button>-</button>
-                            <input type="number" />
-                            <button>+</button>
+                            <button className="button">-</button>
+                            <input type="number" className="input" />
+                            <button className="button">+</button>
                         </div>
-                        <div className="shop-total">總計</div>
-                        <div className="shop-del">刪除</div>
+                        <div className="shop-total">$200000</div>
+                        <div className="shop-del">
+                            <button className="button">刪除</button>
+                        </div>
+                    </div>
+                    <div className="shop-car">
+                        <div className="shop-car-chk"><input type="checkbox" /></div>
+                        <div className="shop-img">
+                            <img src=""/>
+                        </div>
+                        <div className="shop-text">在台現貨/贈10禮 免運 可到付 酷貝樂/艾尚寶 高景觀 可坐可躺 雙向推在台現貨/贈10禮 免運 可到付 酷貝樂/艾尚寶 高景觀 可坐可躺 雙向推</div>
+                        <div className="shop-format">
+                            規格
+                        </div>
+                        <div className="shop-unitprice">$245000</div>
+                        <div className="shop-mount">
+                            <button className="button">-</button>
+                            <input type="number" className="input" />
+                            <button className="button">+</button>
+                        </div>
+                        <div className="shop-total">$200000</div>
+                        <div className="shop-del">
+                            <button className="button">刪除</button>
+                        </div>
+                    </div>
+                    <div className="shop-car">
+                        <div className="shop-car-chk"><input type="checkbox" /></div>
+                        <div className="shop-img">
+                            <img src=""/>
+                        </div>
+                        <div className="shop-text">在台現貨/贈10禮 免運 可到付 酷貝樂/艾尚寶 高景觀 可坐可躺 雙向推在台現貨/贈10禮 免運 可到付 酷貝樂/艾尚寶 高景觀 可坐可躺 雙向推</div>
+                        <div className="shop-format">
+                            規格
+                        </div>
+                        <div className="shop-unitprice">$245000</div>
+                        <div className="shop-mount">
+                            <button className="button">-</button>
+                            <input type="number" className="input" />
+                            <button className="button">+</button>
+                        </div>
+                        <div className="shop-total">$200000</div>
+                        <div className="shop-del">
+                            <button className="button">刪除</button>
+                        </div>
                     </div>
                     <div className="shop-coupon">
                         <div className="shop-coupon-header">
-                        <RiCoupon2Fill/>
-                        全站折價券
-                            <button className="button">選擇折價券</button>
+                            <RiCoupon2Fill/>
+                            <span>全站折價券</span>
+                            <div className="button">選擇折價券</div>
                         </div>
                         <div className="shop-coupon-content">
-                            <div>
-                                <RiCoupon3Fill/>
-                                <p>蝦皮卷</p>
+                            <div className="shop-car-chk"><input type="checkbox" /></div>
+                            <div className="shop-coupon-name">
+                                <RiCoupon3Fill className="coupon-icon"/>
+                                <span>蝦皮卷</span>
                             </div>
-                            <div>$1000</div>
+                            <div>-$1000</div>
                         </div>
                         <div className="shop-coupon-footer">
                             <div className="shop-select">
-                                <input type="checkbox" />
-                                <div>全選</div>
+                                <div className="shop-car-chk"><input type="checkbox" /></div>
+                                <span>全選</span>
                             </div>
                             <div className="shop-result">
                                 <div className="shop-money">
-                                    <div>購買總金額 : </div>
-                                    <span>$0</span>
-                                    <div>可獲得的X幣 : 0 X幣</div>
+                                    <div>購買總金額 : <span>$0</span></div>
+                                    <p>可獲得的X幣 : 0 X幣</p>
                                 </div>
-                                <button className="button">去買單</button>
+                                <button className="button is-danger">去買單</button>
                             </div>
                         </div>
                     </div>
