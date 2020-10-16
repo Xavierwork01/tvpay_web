7c08ddbe87cc80ab4d6223ea80f1c424973ad05f
xavier <xavierwork01@gmail.com>
2020-10-16 16:14:53 +0800

    10/16 last update

--- src/components/User/UserAccount.js
+++ src/components/User/UserAccount.js
@@ -8,7 +8,11 @@ class UserAccount extends React.Component{
         yeararr:[],
         mountharr:[],
         dayarr:[],
-        chooseyear:''
+        chooseyear:'',
+        Userbirthday:'1995/12/12',
+        Useryear:'',
+        Usermounth:'',
+        Userday:''
 
     }
 
@@ -22,6 +26,8 @@ class UserAccount extends React.Component{
     
 
     Mounthselect = (e) =>{
+
+        console.log(e.target.value)
 
         if(e.target.value == 1 || e.target.value == 3 || e.target.value == 5 || e.target.value == 7 || e.target.value == 8 || e.target.value == 10 || e.target.value == 12){
 
@@ -53,7 +59,7 @@ class UserAccount extends React.Component{
 
             }
 
-        } else{
+        } else if(e.target.value == 4 || e.target.value == 6 || e.target.value == 9 || e.target.value == 11){
 
             for(var m = 1; m <= 30;m++){
 
@@ -67,8 +73,29 @@ class UserAccount extends React.Component{
 
     }
 
+    componentDidMount(){
 
-    render(){
+        // user的生日取值
+        let userbirthday = this.state.Userbirthday
+
+        userbirthday = userbirthday.replace(/\//g,'')
+
+        let useryear = userbirthday.substr(0,4)
+        let usermounth = userbirthday.substr(4,2)
+        let userday = userbirthday.substr(6,2)
+
+        usermounth = usermounth.replace(/^0/,'')
+        userday = userday.replace(/^0/,'')
+
+        // 將user的生日放入狀態
+
+        this.setState({
+            Useryear : useryear,
+            Usermounth : usermounth,
+            Userday : userday
+        })
+
+        // 渲染生日選擇器
 
         let newdate = new Date()
 
@@ -86,7 +113,53 @@ class UserAccount extends React.Component{
 
         }
 
-        
+        if(this.state.Usermounth == 1 || this.state.Usermounth == 3 || this.state.Usermounth == 5 || this.state.Usermounth == 7 || this.state.Usermounth == 8 || this.state.Usermounth == 10 || this.state.Usermounth == 12){
+
+            for(var m = 1; m <= 31;m++){
+
+                this.state.dayarr.push(m)
+
+                
+            }
+
+        } else if(this.state.Usermounth == 2){
+
+            if((this.state.chooseyear % 4 == 0 && this.state.chooseyear != 0) || (this.state.chooseyear % 400 == 0)){
+
+                for(var m = 1; m <= 29;m++){
+
+                    this.state.dayarr.push(m)
+    
+                }
+
+            } else{
+
+                for(var m = 1; m <= 28;m++){
+
+                    this.state.dayarr.push(m)
+    
+                }
+
+
+            }
+
+        } else if(this.state.Usermounth == 4 || this.state.Usermounth == 6 || this.state.Usermounth == 9 || this.state.Usermounth == 11){
+
+            for(var m = 1; m <= 30;m++){
+
+                this.state.dayarr.push(m)
+
+            }
+
+        }
+
+
+    }
+
+
+    render(){
+
+    
 
         return(
             <>
@@ -129,16 +202,19 @@ class UserAccount extends React.Component{
                         <div className="account-items">
                             <label>生日</label>
                             <select name="years" className="select" onChange={this.Yearselect}>
+                                <option>{this.state.Useryear}</option>
                                 {this.state.yeararr.map(v => (
                                     <option value={v}>{v}</option>
                                 ))}
                             </select>
                             <select name="mounth" className="select" onChange={this.Mounthselect}>
+                                <option>{this.state.Usermounth}</option>
                                 {this.state.mountharr.map(v => (
                                     <option value={v}>{v}</option>
                                 ))}
                             </select>
                             <select name="day" className="select">
+                                <option>{this.state.Userday}</option>
                                 {this.state.dayarr.map(v => (
                                     <option value={v}>{v}</option>
                                 ))}
@@ -149,22 +225,22 @@ class UserAccount extends React.Component{
                         </div>
                     </div>
                     <div className="account-sticker">
-                        <div class="file has-name is-boxed is-small is-primary">
                         <div className="sticker-img">
-                            <img src="" />
+                            <img src="/images/2.jpg" />
                         </div>
-                            <label class="file-label">
-                                <input class="file-input" type="file" name="resume"/>
-                                <span class="file-cta">
-                                <i class="fas fa-upload"></i>
-                                <span class="file-label">
-                                    選擇圖片
-                                </span>
-                                </span>
-                                <span class="file-name">
-                                Screen Shot 2017-07-29 at 15.54.25.png
-                                </span>
-                            </label>
+                        <div class="file has-name is-boxed is-small is-primary">
+                        <label class="file-label">
+                            <input class="file-input" type="file" name="resume"/>
+                            <span class="file-cta">
+                            <i class="fas fa-upload"></i>
+                            <span class="file-label">
+                                選擇圖片
+                            </span>
+                            </span>
+                            <span class="file-name">
+                            Screen Shot 2017-07-29 at 15.54.25.png
+                            </span>
+                        </label>
                         </div>
                         <p>檔案格式 : .JPEG、.PNG</p>
                     </div>
